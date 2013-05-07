e2249dba96f2f114b94cde163a7f8521ba3973ff
Erik Pfleiderer <erik@smartcoffeetech.com>
2013-05-06 18:49:35 -0700

    updated typeahead stuff
    submits coffee id

--- coffeepost/www/php/favorite_submit.php
+++ coffeepost/www/php/favorite_submit.php
@@ -1,7 +1,11 @@
 <?php
 ini_set('display_errors', 1);
-$name = $_POST['name'];
-
+$fav_coffee = $_POST['fav_coffee'];
+$coffee_id = $_POST['coffee_id'];
+echo " Coffee Name: ";
+echo $fav_coffee;
+echo "   Coffee ID: ";
+echo $coffee_id;
 //specify desired url
-header( "Location: ../recommendation.html" );
+/*header( "Location: ../recommendation.html" );*/
 ?>
