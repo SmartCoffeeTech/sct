faeb0804e8bd5c8ee025ef7d0cce75e146e8280a
Kevin Perko <kperko@smartcoffeetech.com>
2013-05-09 19:24:59 -0700

    added func for parsing querystring

--- coffeepost/www/script/recommendation.js
+++ coffeepost/www/script/recommendation.js
@@ -1,3 +1,18 @@
+(function($) {
+    $.QueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=');
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
+
+
 function clicked(item) {
 	var id;
     id = $(item).attr("id");
@@ -5,8 +20,8 @@ function clicked(item) {
 }
 
 $.ajax({ 
-url: "data/dataout.json", 
-cache: false,
+url: "/tmp/dataout"+time_epoch+".json", 
+cache: true,
 	success: function(data){
 		$("#coffee").find("description").text(data.coffee.coffee_description);
 		$("#coffee").find("aromas").text(data.coffee.coffee_aromas);
@@ -16,7 +31,7 @@ cache: false,
 		$("#roast_image").attr("src", data.coffee.roast_image_url);
 	},
 	dataType: "json",
-	cache: false
+	cache: true
 });
 Shadowbox.init();
 
